389f3ee7da30322fb5c386d7f57816d4709f3e41
Sonam Samdupkhangsar <sonamhava@gmail.com>
2023-10-02 14:44:52 -0600

    Feature/spring authorization server (#1)
    * login working but callback not
    
    * token mediator flow get token working
    
    * remove client secret not used
    
    * token refresh working
    
    * commit
    
    * set token userRole to admin by parsing accessToken
    
    ---------
    
    Co-authored-by: Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>

--- README.md
+++ README.md
@@ -89,6 +89,11 @@ To run your site locally, use:
 npm run dev
 ```
 
+To run on a port like 3001:
+```
+npm run dev -- -p 3001
+```
+
 To run it in production mode, use:
 
 ```
@@ -111,3 +116,26 @@ Follow the [Deployment documentation](https://next-auth.js.org/deployment)
 
 ISC
 
+## NextJs token creation procecc
+1. send a browser request to 
+```
+http://my-server:9001/oauth2/authorize?response_type=code&scope=openid%20profile%20email&client_id=nextjs-client&redirect_uri=http://localhost:3001/api/auth/callback/myauth
+```
+The above should show a login page.  Enter the correct username/password 
+which will redirect in the browser itself with the code.
+
+Copy the code and paste in postman:
+```
+http://localhost:9001/oauth2/token?grant_type=authorization_code&redirect_uri=http://localhost:3001/api/auth/callback/myauth&code=1MYAIujmQssFNqIx5_xY_IAS0sYMDXMzFda2PWvDa0cPwmc2LOKJS6xSNzNSp0Cfq17T5bnkMMApvP6tD4VESC6Eq5imX4AilH7lCITK1RU4jPkEawrjNyLy5yfOlqDy&scope=openid%20email%20profile
+```
+
+Click the `Send` button to fire the request and it should come back with following response:
+```
+{
+    "access_token": "eyJraWQiOiI1Yjk0ZGVjYS0wYjg2LTQ5ZjctYjY1Ny1kMThmNjU1NTEyNjMiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJzb25hbSIsImF1ZCI6Im5leHRqcy1jbGllbnQiLCJuYmYiOjE2OTA2NTQ0NDcsInNjb3BlIjpbIm9wZW5pZCIsInByb2ZpbGUiLCJlbWFpbCJdLCJpc3MiOiJodHRwOi8vbXktc2VydmVyOjkwMDEiLCJleHAiOjE2OTA2NTQ3NDcsImlhdCI6MTY5MDY1NDQ0N30.KaltK2mddWzO1ksLVe-X2CyTqV2E_1N5t_gCfowD3gsoCRZF39rNWflwFb8DYwhCCuiyPplaP-CQ-uduJPi_ysgiTTKh3DkcPod0vE7quU83i4HYOYcJZu5rqOS8_3Vbr1EwXUODbD12v9g-em8ZWvwthGqJwZoD1hYBzOEsL1792TGBrvKuBvE2ZJ9VcwOwLrHV9qaZx45jKFnx_SyiIJc22QIgvY3lOwfiSmxyHMY74bTKHgNa8RHT-DaQR-tJNxBMZuHW80V1YZ1-b0KFwVQV2FiH2jxv3Rg0K6dUd0ksbD86HxsFUJWK9RSIRQtcFvU0vKMHwCrY8roACXbHWw",
+    "scope": "openid profile email",
+    "id_token": "eyJraWQiOiI1Yjk0ZGVjYS0wYjg2LTQ5ZjctYjY1Ny1kMThmNjU1NTEyNjMiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJzb25hbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiaXNzIjoiaHR0cDovL215LXNlcnZlcjo5MDAxIiwicGhvbmVfbnVtYmVyX3ZlcmlmaWVkIjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoic29uYW0iLCJnaXZlbl9uYW1lIjoic29uYW0iLCJzaWQiOiJIYUkxS3dlVm1aRVhXZWJVQ0pzRXRZY1Q3bVhXN0ZkcldHRVc0SDVNR2pVIiwiYXVkIjoibmV4dGpzLWNsaWVudCIsImF6cCI6Im5leHRqcy1jbGllbnQiLCJhdXRoX3RpbWUiOjE2OTA2NTQ0MTYsIm5hbWUiOiJzb25hbSIsImV4cCI6MTY5MDY1NjI0NywiaWF0IjoxNjkwNjU0NDQ3LCJmYW1pbHlfbmFtZSI6InNhbWR1cGtoYW5nc2FyIiwiZW1haWwiOiJtZUBzb25hbS5lbWFpbCJ9.i1AkFdND6RfoM9DIN0eojH0WnqsW_bxuUP34fI1y34KqKM2nXTajoL-qF4PoNtZSH3E4g4oCLoUVPYxXHWXKbYoMwdE9fo-00hwDwZ8WtTibCQC6Sha9m0VHpOIZqHsqEXHJtcu3jxHRAXmAHrhdMP-SKy8BYZVkoRBt92bnGgmmCBNJKTRGE1XWtOqXnH9_24DcTyy_Wlpuq02gLp8TaebCRc8JMnw2QeWUc1Loe1xDgzphcR4KJLRqBwXu-m_NCIbcKb3k9kHWDvPJET62ZByOXBE96rJJIVevHzEAOZie_5LWgx3TOD6x49xSHmgGZvYt9kSQJm6YQL9eaqAyqQ",
+    "token_type": "Bearer",
+    "expires_in": 299
+}
+```
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,6 +4,20 @@ import FacebookProvider from "next-auth/providers/facebook"
 import GithubProvider from "next-auth/providers/github"
 import TwitterProvider from "next-auth/providers/twitter"
 import Auth0Provider from "next-auth/providers/auth0"
+import { useParams } from 'next/navigation'
+import { url } from "inspector"
+import { URLSearchParams } from "url"
+import { useSearchParams } from 'next/navigation';
+//import { request } from "http"
+import { OAuthChecks, OAuthConfig } from "next-auth/providers"
+import { CallbackParamsType, BaseClient } from "openid-client"
+import { NextResponse } from "next/server"
+import { Auth } from "@auth/core"
+import { type TokenSet } from "@auth/core/types"
+import { JWT, getToken } from "next-auth/jwt"
+import jwt_decode from 'jwt-decode'
+import { parse } from "path"
+
 // import AppleProvider from "next-auth/providers/apple"
 // import EmailProvider from "next-auth/providers/email"
 
@@ -50,16 +64,168 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.AUTH0_SECRET,
       issuer: process.env.AUTH0_ISSUER,
     }),
+    {
+      id: "myauth",
+      name: "myauthname",
+      type: "oauth",
+      clientId: "nextjs-client",
+      authorization: {
+        url: "http://localhost:8080/oauth2-token-mediator/authorize",
+        params: { scope: "openid email profile" }
+       },       
+      token: {
+        url: "http://localhost:8080/oauth/token", 
+
+        async request(context) {
+          console.log("code: %s, redirect_uri: %s", context.params.code, context.params.redirect_uri)
+          const tokens = await makeTokenRequest(context)          
+          console.log('tokens: {}', tokens)
+          return { tokens }
+        }         
+      },
+     
+      idToken: true,      
+      checks: ["pkce", "state"],
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture          
+        }
+      },
+    }
   ],
   theme: {
     colorScheme: "light",
   },
   callbacks: {
-    async jwt({ token }) {
-      token.userRole = "admin"
+    async jwt({ token, user, account }) {
+      console.log("user: ", user)
+
+      
+      
+      token.userRole = "admin"      
+      console.log('token: ', token)    
+      
+      if (account) {
+        console.log('account: ', account)
+        
+        token = Object.assign({}, token, { access_token: account.access_token, refresh_token: account.refresh_token });        
+        var map = jwt_decode("" + account.access_token) as any
+        console.log("parse jwt token: ", map)
+        console.log("userRole: ", map.userRole)
+        var userRoles = <Array<string>> map.userRole
+        if (userRoles.includes('admin')) {
+          token.userRole = "admin";
+          console.log("set token.userRole to admin");
+        }
+        
+        
+
+        console.log("account expires at: ", account.expires_at)
+       // token.refreshToken = account.refresh_token        
+       // token.accessToken = account.access_token
+        if (account.refresh_token) {
+          token.accessTokenExpires =  account.expires_at!  * 1000 //seconds * 1000 = milliseconds         
+        }
+      }
+
+      // If token has not expired, return it,
+      if (Date.now() <  (Number(token.accessTokenExpires))) {
+        //Date.now() returns number of milliseconds since epoch
+        console.log("token.accessTokenExpires is not expired, Date.now(): ", Date.now(),
+        ", token.accessTokenExpires: ", token.accessTokenExpires)
+       // return token
+      }
+
+      // Otherwise, refresh the token.
+      var tokens = await refreshAccessToken(token)
+      console.log('token from refresh: ', tokens)
+      token = Object.assign({}, token, { access_token: tokens.access_token, refresh_token: tokens.refresh_token });        
       return token
+    
+      
     },
+
+    async session({session, token}) {
+      if(session) {
+        session = Object.assign({}, session, {access_token: token.access_token})
+        console.log('session: ', session);
+        }
+      return session
+      }
   },
 }
 
+declare module "@auth/core/types" {
+  interface Session {
+    error?: "RefreshAccessTokenError"
+  }
+}
+
+declare module "@auth/core/jwt" {
+  interface JWT {
+    access_token: string
+    expires_at: number
+    refresh_token: string
+    //add following to test if it works
+    userRole: string
+    error?: "RefreshAccessTokenError"
+  }
+}
+
 export default NextAuth(authOptions)
+
+async function makeTokenRequest(context: { params: CallbackParamsType; checks: OAuthChecks } & { client: BaseClient; provider: OAuthConfig<{ [x: string]: unknown }> & { signinUrl: string; callbackUrl: string } }) {
+  console.log("params: ",context.params)
+  
+  const request = await fetch('http://localhost:8080/oauth2-token-mediator/token?grant_type='
+    +'authorization_code&code='+context.params.code
+    +'&redirect_uri=http://localhost:3001/api/auth/callback/myauth'
+    +'&scope=openid%20email%20profile', {
+           
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',            
+              'client_id': 'nextjs-client'
+            }
+          }).then( function(response) {
+            return response.json();
+          }).then(function(data) {          
+            return data;
+          });
+          return request;
+}
+
+/**
+ * Takes a token, and returns a new token with updated
+ * `accessToken` and `accessTokenExpires`. If an error occurs,
+ * returns the old token and an error property
+ */
+async function refreshAccessToken(token: any) {
+  console.log('refresh token: ', token.refresh_token);  
+  const url =
+      "http://localhost:8080/oauth2-token-mediator/token?" +
+      new URLSearchParams({
+        client_id: "nextjs-client",        
+        grant_type: "refresh_token",
+        refresh_token: token.refresh_token      
+      })
+
+  const response = await fetch(url, {
+    headers: {
+      "client_id": "nextjs-client"
+    },
+    method: "POST",
+  }).then(function(response) {
+      if (!response.ok)
+        throw new Error("failed to refresh token")
+      else  
+        return response.json()
+    }).then(function(data) {
+      return data;
+    })
+    return response;
+}
+
